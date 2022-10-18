1422d6843e36d31106bdc06bfe3889abbb662fcd
SatteyChauhan <satyakarchauhan@outlook.com>
2022-10-19 00:45:22 +0530

    code

--- Home.js
+++ Home.js
@@ -7,3 +7,8 @@ function closeCoupon(){
     document.getElementById('coupon').style.visibility = 'hidden';
     document.getElementById('main').style.opacity='1'
 }
+
+function changeMode(){
+    let mybody = document.body;
+    mybody.classList.toggle('mydark')
+}
